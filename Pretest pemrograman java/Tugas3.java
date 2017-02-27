97132d97b963d0088801b263fdc10f1c2be0ba5c
dediwidartoo <dediwidarto78@gmail.com>
2017-02-27 10:01:06 +0700

    Created by Coding Ground Client

---
+++ Pretest pemrograman java/Tugas3.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class Tugas3{
+
+    public static void main(String []args){
+        Scanner inputdata = new Scanner (System.in);
+        int tampilAngka;
+        System.out.print("masukkan angka sembarang : ");
+        tampilAngka = inputdata.nextInt();
+        System.out.println("Angka yang anda masukkan adalah "+tampilAngka);
+    }
+}
