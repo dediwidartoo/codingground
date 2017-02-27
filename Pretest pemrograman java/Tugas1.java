337b7e885dcb56ac11a424c48227071a7d9b2f3d
dediwidartoo <dediwidarto78@gmail.com>
2017-02-27 08:40:07 +0700

    Created by Coding Ground Client

---
+++ Pretest pemrograman java/Tugas1.java
@@ -0,0 +1,9 @@
+public class Tugas1{
+
+     public static void main(String []args){
+         String nama = "Dedi Widarto";
+         String kota = "Jepara";
+        System.out.println("Nama : "+nama);
+        System.out.println("Kota asal : "+kota);
+     }
+}
