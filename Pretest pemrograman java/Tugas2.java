05edc94e2ffc27af60edce42b8fc36a8074de303
dediwidartoo <dediwidarto78@gmail.com>
2017-02-27 10:01:47 +0700

    Created by Coding Ground Client

---
+++ Pretest pemrograman java/Tugas2.java
@@ -0,0 +1,13 @@
+public class Tugas2{
+    
+    public static void main(String []args){
+        String[] nama;
+        nama = new String[3];
+        nama[0] = "Dedi Widarto";
+        nama[1] = "Ade Danvi";
+        nama[2] = "Sukron Mugajir";
+        for(int i = 0; i < nama.length; i++){
+            System.out.println(nama[i]);
+        }
+    }
+}
